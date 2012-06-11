35d458ad0d19f69eae24705495092b6fb6bbe879
swaudo <simemaw30@gmail.com>
2012-06-11 21:52:04 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smesh::Application.config.secret_token = 'd7cdb51995566aed0f4e23a3aee6a4809abdd14d7d8cdf1192cfe05ae48c7cbfb15c6e227ded9406d992e4634134a8e39b17c58ed1dfd3f5f72a3cc7929fb4c2'
